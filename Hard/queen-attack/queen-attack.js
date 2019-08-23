a947c8581bf225f8b8e950bf7816add2f42793f7
Andrew Bieneman <acbieneman@gmail.com>
2019-08-23 12:35:44 -0600

    added hard queen-attack

---
+++ Hard/queen-attack/queen-attack.js
@@ -0,0 +1,139 @@
+export class QueenAttack {
+    constructor(input) {
+        this.white = [0, 3];
+        this.black = [7, 3];
+    
+        if(input) {
+            this.white = input.white;
+        }
+        if(input) {
+            this.black = input.black;
+        }
+        if( (this.black[0] == this.white[0]) && (this.black[1] == this.white[1]) ) {
+            throw('Queens cannot share the same space');
+        }
+
+        this.rows = [];
+
+        let rowString = "_ _ _ _ _ _ _ _"
+        for(let i = 0; i < 8; i++) {
+            this.rows[i] = rowString;
+        }
+
+        let whiteString;
+        switch(this.white[1]) {
+            case 0:
+                whiteString = "W _ _ _ _ _ _ _"
+                break;
+            
+            case 1:
+                whiteString = "_ W _ _ _ _ _ _"
+                break;
+
+            case 2:
+                whiteString = "_ _ W _ _ _ _ _"
+                break;
+
+            case 3:
+                whiteString = "_ _ _ W _ _ _ _"
+                break;
+
+            case 4:
+                whiteString = "_ _ _ _ W _ _ _"
+                break;
+                
+            case 5:
+                whiteString = "_ _ _ _ _ W _ _"
+                break;
+
+            case 6:
+                whiteString = "_ _ _ _ _ _ W _"
+                break;
+
+            case 7:
+                whiteString = "_ _ _ _ _ _ _ W"
+                break;            
+        }
+
+        let blackString;
+        switch(this.black[1]) {
+            case 0:
+                blackString = "B _ _ _ _ _ _ _"
+                break;
+            
+            case 1:
+                blackString = "_ B _ _ _ _ _ _"
+                break;
+
+            case 2:
+                blackString = "_ _ B _ _ _ _ _"
+                break;
+
+            case 3:
+                blackString = "_ _ _ B _ _ _ _"
+                break;
+
+            case 4:
+                blackString = "_ _ _ _ B _ _ _"
+                break;
+                
+            case 5:
+                blackString = "_ _ _ _ _ B _ _"
+                break;
+
+            case 6:
+                blackString = "_ _ _ _ _ _ B _"
+                break;
+
+            case 7:
+                blackString = "_ _ _ _ _ _ _ B"
+                break;            
+        }
+
+        this.rows[this.white[0]] = whiteString;
+        this.rows[this.black[0]] = blackString;
+    }
+
+    toString() {
+        let returnString = "";
+        for(let i = 0; i < 8; i++) {
+            returnString += this.rows[i];
+            returnString += '\n';
+        }
+        return returnString;
+    }
+
+    canAttack() {
+        if( (this.white[0] == this.black[0]) || (this.white[1] == this.black[1]) ) {
+            return true;
+        }
+        
+        if(  (this.white[0] + this.white[1]) == (this.black[0] + this.black[1]) ) {
+            return true;
+        }
+
+        let y = this.white[0];
+        let x = this.white[1];
+
+        while( (y < 8) && (x < 8) ) {
+            if( (y = this.black[1]) && (x == this.black[0]) ) {
+                return true;
+            }
+            y++;
+            x++;
+        }
+
+        y = this.white[0];
+        x = this.white[1];
+
+        while( (y >= 0) && (x >= 0) ) {
+            if( (y = this.black[1]) && (x == this.black[0]) ) {
+                return true;
+            }
+            y--;
+            x--;
+        }
+
+        return false;
+    }
+}
